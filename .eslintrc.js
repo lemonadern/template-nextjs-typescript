b81339ebb08ecd64283a05f742109e91ec761713
lemonadern <taishi.develop@gmail.com>
2022-04-22 21:18:08 +0900

    lint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,30 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-    'prettier',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'import',
-  ],
-  'rules': {
-    'semi': ['error', 'never'],
+  plugins: ['react', '@typescript-eslint', 'import'],
+  rules: {
+    semi: ['error', 'never'],
     'require-jsdoc': ['off'],
-    'import/order': ['error', {
-      'newlines-between': 'always',
-      'alphabetize': {
-        'order': 'asc',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
       },
-    }],
+    ],
   },
 }
