03c9dcca2bd8619aee5818ea3db28e1c679f5368
lemonadern <taishi.develop@gmail.com>
2022-04-23 14:00:01 +0900

    change prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   printWidth: 100,
-  trailingComma: 'es5',
+  trailingComma: 'all',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   bracketSpacing: true,
   jsxSingleQuote: true,
+  arrowParens: 'avoid',
 }
